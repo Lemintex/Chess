d30c14499b23829e6db54d775ba1a3f62efaa38c
Lemintex <aaron_nobile@hotmail.co.uk>
2021-05-26 23:55:34 +0100

    Starting position added

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ function preload(){
 function setup() {
   chessBoard = new ChessBoard(squareSize);
   pieceController = new PieceController(spriteSheet, squareSize, chessBoard);
-  var fen = new PositionFromFEN('kKnNpPqQ/bBrR4/kkK', 8, 8, pieceController);
+  var fen = new PositionFromFEN('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR', 8, 8, pieceController);
   createCanvas(squareSize*8, squareSize*8);
   // pieceController.generatePieces();
 }
